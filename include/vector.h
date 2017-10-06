f2a84a6e228a7a8a7100f59d95945ac636dec30a
Ravi Agrawal <ravi.agrwl420@gmail.com>
2017-10-06 16:53:00 -0700

    Move Vector method implementations to vector.cpp

--- include/vector.h
+++ include/vector.h
@@ -8,26 +8,15 @@ struct Vector {
 	Vector () {};
 	Vector (float x, float y, float z) : x(x), y(y), z(z) {};
 
-	Vector* operator+ (const Vector &vec) const {
-		return new Vector(x + vec.x, y + vec.y, z + vec.z);
-	};
+	Vector* operator+ (const Vector &vec) const;
 
-	Vector* operator- (const Vector &vec) const {
-		return new Vector(x - vec.x, y - vec.y, z - vec.z);
-	};
+	Vector* operator- (const Vector &vec) const;
 
-	float length () const {
-		return sqrtf(powf(x, 2) + powf(y, 2) + powf(z, 2));
-	};
+	float length () const;
 
-	Vector* normalize () const {
-		float length = this->length();
-		return new Vector(x/length, y/length, z/length);
-	};
+	Vector* normalize () const;
 
-	Vector* scalar_mult (float scalar) const {
-		return new Vector(x * scalar, y * scalar, z * scalar);
-	};
+	Vector* scalar_mult (float scalar) const;
 };
 
 Vector* cross (const Vector&, const Vector&);
--- src/vector.cpp
+++ src/vector.cpp
@@ -1,5 +1,26 @@
 #include <vector.h>
 
+Vector* Vector::operator+ (const Vector &vec) const {
+	return new Vector(x + vec.x, y + vec.y, z + vec.z);
+};
+
+Vector* Vector::operator- (const Vector &vec) const {
+	return new Vector(x - vec.x, y - vec.y, z - vec.z);
+};
+
+float Vector::length () const {
+	return sqrtf(powf(x, 2) + powf(y, 2) + powf(z, 2));
+};
+
+Vector* Vector::normalize () const {
+	float length = this->length();
+	return new Vector(x/length, y/length, z/length);
+};
+
+Vector* Vector::scalar_mult (float scalar) const {
+	return new Vector(x * scalar, y * scalar, z * scalar);
+};
+
 Vector* cross (const Vector &vec1, const Vector &vec2) {
 	return new Vector(vec1.y * vec2.z - vec1.z * vec2.y, vec1.z * vec2.x - vec1.x * vec2.z, vec1.x * vec2.y - vec1.y * vec2.x);
 };
